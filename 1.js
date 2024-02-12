dd653bff801d0dd4d463c6993dd70cfbe4a25ad5
kinghassaan7 <145790867+kinghassaan7@users.noreply.github.com>
2024-02-12 21:51:23 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,65 @@
+// 1. sum of all digits of a number
+
+// let userInput = prompt("Enter digits")
+// let number = parseInt(userInput);
+
+// let str = number.toString();
+
+// let sum = 0;
+
+// for (let i = 0; i < str.length; i++) {
+//     sum += parseInt(str[i])   
+// }
+
+// console.log(`Sum = ${sum}`)
+
+// ====================================
+
+// 2. sum of range of 2 numbers
+
+// let num1 = +prompt("First number")
+// let num2 = +prompt("Second number")
+
+// let sum = 0;
+
+// for (let i = num1; i <= num2; i++) {
+//     sum += i;  
+// }
+
+// console.log(sum)
+
+// ====================================
+
+// 3. average
+
+// const numbers = [5, 10, 15, 20];
+// const average = numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
+
+// console.log("Average:", average);
+
+// let average = numbers.reduce((iter,adds) => iter + adds,0) 
+
+// ====================================
+
+// 4. compare strings by count of characters
+
+// let str1 = prompt("Enter first word: ")
+// let str2 = prompt("Enter second word: ")
+
+// if (str1.length === str2.length) {
+//     console.log("The characters are equal.")
+// }
+// else{
+//     console.log("The characters are not equal.")
+// }
+
+// 5. name -> eamn
+
+// let word = prompt("Enter a word: ");
+
+// let fchar = word[0];
+// let lchar = word[word.length -1]
+
+// word = lchar + word.slice(1, -1) + fchar;
+
+// console.log(word)
